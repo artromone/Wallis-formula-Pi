e8301c750ec061c71f13ef63e2032f9d469b792e
Artem <artrom170@gmail.com>
2023-05-31 13:33:24 +0300

    constexpr

--- main.cpp
+++ main.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
-long long doubFact(long long n)
+constexpr unsigned long long doubFact(unsigned long long n)
 {
   if (n == 2 || n == 1)
   {
     return n;
   }
-  long long result = 1;
+  unsigned long long result = 1;
   size_t c = (n % 2 == 0) ? 2 : 3;
-  for (long long i = n; i >= c; i -= 2)
+  for (auto i = n; i >= c; i -= 2)
   {
     result *= i;
   }
   return result;
 }
-double toSquare(double n)
+constexpr double toSquare(double n)
 {
   return n * n;
 }
+constexpr double calcPi(unsigned long long n)
+{
+  auto a = static_cast< double >(doubFact(2 * n));
+  auto b = static_cast< double >(doubFact(2 * n - 1));
+  return toSquare(a / b) * (2.0 / static_cast< double >(2 * n + 1));
+}
 int main()
 {
-  long long n = 15;
-  auto a = static_cast<double>(doubFact(2 * n));
-  auto b = static_cast<double>(doubFact(2 * n - 1));
-  double res = toSquare(a / b) * (2.0 / static_cast<double>(2 * n + 1));
-  std::cout << a << '\n' << b << '\n' << res; //3.09134
+  std::cout << calcPi(15); //3.09134
   return 0;
 }
