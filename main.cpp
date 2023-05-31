db0403b44e1a457b751dfb2afb8d3a5308f8da1e
Artem <artrom170@gmail.com>
2023-05-31 12:56:23 +0300

    toSquare() function, calc result

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include "iostream"
-int doubleFactorial(int n)
+double doubFact(double n)
 {
   if (n == 2 || n == 1)
   {
     return n;
   }
-  return n * doubleFactorial(n - 2);
+  return n * doubFact(n - 2);
+}
+double doubFact(size_t n)
+{
+  return doubFact(static_cast< double >(n));
+}
+double toSquare(double n)
+{
+  return n * n;
 }
 int main()
 {
-  std::cout << doubleFactorial(10);
+  size_t n = 100;
+  auto res =
+    toSquare(doubFact(2 * n) / static_cast< double >(doubFact(2 * n - 1)))
+    * (2 / static_cast< double >(2 * n + 1));
+  std::cout << res;
 }
