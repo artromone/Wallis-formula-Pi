46c88bfe8c2122e0138707bf9e1737d511c24504
Artem <artrom170@gmail.com>
2023-05-31 12:39:32 +0300

    doubleFactorial() function

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+#include "iostream"
+int doubleFactorial(int n)
+{
+  if (n == 2 || n == 1)
+  {
+    return n;
+  }
+  return n * doubleFactorial(n - 2);
+}
 int main()
 {
+  std::cout << doubleFactorial(10);
 }
